f662ec5ebd23f277231952b6869635a3ac31b322
Furs Andrei <you@example.com>
2022-11-08 11:51:37 +0300

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert(
+            [
+                [
+                    'name'=>'Admin', 
+                    'email'=>'admin@admin.com', 
+                    'email_verified_at'=>Carbon::now(), 
+                    'password'=>bcrypt('admin@admin.com'), 
+                    'remember_token' => Str::random(10),
+                ],
+            ]
+        );
+    }
+}
