2282d9a9897befa1248470447b91f4c0b24dc356
Furs Andrei <you@example.com>
2022-11-13 17:03:12 +0300

    refactoring reset images

---
+++ app/Actions/ResetImagesAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Actions;
+
+use Illuminate\Support\Facades\Storage;
+
+class ResetImagesAction
+{
+    public function __invoke(string $folderTo, string $folderFrom)
+    {
+        Storage::disk('public')->deleteDirectory($folderTo);
+        Storage::disk('public')->makeDirectory($folderTo);
+
+        $files = Storage::disk('public')->files($folderFrom);
+        foreach ($files as $fileName) {
+            $content = Storage::disk('public')->get($fileName);
+            $fileName = str_replace($folderFrom, $folderTo, $fileName);
+            Storage::disk('public')->put($fileName, $content);
+        }
+    }
+}
--- app/Actions/ResetProjectAction.php
+++ app/Actions/ResetProjectAction.php
@@ -11,16 +11,6 @@ class ResetProjectAction
     {
         Artisan::call('migrate:fresh --seed');
         
-        $folder = 'uploads';
-        $folder_reset = 'reset_img';
-        Storage::disk('public')->deleteDirectory($folder);
-        Storage::disk('public')->makeDirectory($folder);
-
-        $files = Storage::disk('public')->files($folder_reset);
-        foreach ($files as $file_name) {
-            $contents = Storage::disk('public')->get($file_name);
-            $file_name = str_replace($folder_reset, $folder, $file_name);
-            Storage::disk('public')->put($file_name, $contents);
-        }
+        (new ResetImagesAction)('uploads', 'reset_img');
     }
 }
